e1fea391fcfd5b58e68687b4df0977bb4b3cea47
bendiller <diller.benjamin@gmail.com>
2020-01-21 19:57:36 -0700

    Initialize `Portfolio` model

--- brd_site/portfolio/models.py
+++ brd_site/portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(blank=False, max_length=64, unique=True)
+    start_date = models.DateField(blank=False)
+    stop_date = models.DateField(blank=True)
+    short_blurb = models.TextField(blank=False)
+    long_blurb = models.TextField(blank=False)
+    code_link = models.URLField(blank=True)

da4f601cd8e895854e0cf4d92597059d0de4a498
bendiller <diller.benjamin@gmail.com>
2020-01-25 11:33:04 -0700

    Modify `Project` PK to be name instead of auto-ID for admin readability

--- brd_site/portfolio/models.py
+++ brd_site/portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Project(models.Model):
-    name = models.CharField(blank=False, max_length=64, unique=True)
+    name = models.CharField(blank=False, max_length=64, primary_key=True, unique=True)
     start_date = models.DateField(blank=False)
     stop_date = models.DateField(blank=True)
     short_blurb = models.TextField(blank=False)

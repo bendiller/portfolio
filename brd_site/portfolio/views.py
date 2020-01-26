87f2628ada67b43563cbda3a7d2f9d6acf9a9116
bendiller <diller.benjamin@gmail.com>
2020-01-25 21:26:55 -0700

    Implement initial structure of tiles & cards

--- brd_site/portfolio/views.py
+++ brd_site/portfolio/views.py
@@ -4,6 +4,14 @@ from .models import Project
 
 
 def index(request):
-    context = dict()
-    context['projects'] = Project.objects.order_by('-start_date').values()
+    """Provide Projects as list of 2-tuples for easier 2-column display by template"""
+    proj_list = Project.objects.order_by('-start_date').values()
+    tup_list = list()
+    for i, _ in enumerate(proj_list[::2]):
+        try:
+            tup_list.append((proj_list[2*i], proj_list[2*i + 1]))
+        except IndexError:  # Deals with final iteration when len(proj_list) is odd.
+            tup_list.append((proj_list[2*i], False))
+
+    context = {'projects': tup_list}
     return render(request, 'portfolio/index.html', context)

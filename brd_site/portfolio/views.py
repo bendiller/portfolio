f1bb61238d24be247efadb9682e1906da6dfe185
bendiller <diller.benjamin@gmail.com>
2020-02-04 17:23:53 -0700

    Replace tile-based layout with column-based equivalent

--- brd_site/portfolio/views.py
+++ brd_site/portfolio/views.py
@@ -4,26 +4,18 @@ from .models import Project, TechTag
 
 
 def index(request, tag=None):
-    """Provide Projects as list of 2-tuples for easier 2-column display by template"""
     context = {}
     if tag:
         try:
-            proj_list = TechTag.objects.get(name=tag).projects.all()
+            context['projects'] = TechTag.objects.get(name=tag).projects.all()
             context['tag'] = tag
         except TechTag.DoesNotExist:
             context['bad_tag'] = tag
             return render(request, 'portfolio/index.html', context={'bad_tag': tag})
 
     else:
-        proj_list = Project.objects.order_by('-start_date')
-    tup_list = list()
-    for i, _ in enumerate(proj_list[::2]):
-        try:
-            tup_list.append((proj_list[2*i], proj_list[2*i + 1]))
-        except IndexError:  # Deals with final iteration when len(proj_list) is odd.
-            tup_list.append((proj_list[2*i], False))
+        context['projects'] = Project.objects.order_by('-start_date')
 
-    context['projects'] = tup_list
     return render(request, 'portfolio/index.html', context)
 
 

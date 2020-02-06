69cb01a8111bf706fdc9323b80bcb85daf193575
bendiller <diller.benjamin@gmail.com>
2020-02-06 12:06:22 -0700

    Cleanup Javascript (DRY)

--- brd_site/portfolio/static/portfolio/tabs.js
+++ brd_site/portfolio/static/portfolio/tabs.js
@@ -1,53 +1,29 @@
-var allProj = document.getElementById("all-proj");
-allProj.onclick = function(){
-    console.log("Clicked allProj!");
-    // Show all project cards:
-    projObjects = document.getElementsByClassName("proj");
-    var i;
-    for (i=0; i<projObjects.length; i++) {
-        projObjects[i].style.display = "block";
-    }
-    // Set correct tab active state:
-    document.getElementById("tab-all-proj").classList.add("is-active");
-    document.getElementById("tab-pers-proj").classList.remove("is-active");
-    document.getElementById("tab-open-source-proj").classList.remove("is-active");
+document.getElementById("all-proj").onclick = function() {tabClick("", "proj", "tab-all-proj")};
+document.getElementById("personal-proj").onclick = function() {tabClick("OpenSource", "Personal", "tab-pers-proj")};
+document.getElementById("open-source-proj").onclick = function() {
+    tabClick("Personal", "OpenSource", "tab-open-source-proj")
 };
 
-var personalProj = document.getElementById("personal-proj");
-personalProj.onclick = function(){
-    console.log("Clicked personalProj!");
-    // Show personal project cards:
-    persObjects = document.getElementsByClassName("Personal");
+function tabClick(hideableClassName, showableClassName, activeTab) {
+    // Hide un-selected cards:
+    hideableElements = document.getElementsByClassName(hideableClassName);
     var i;
-    for (i=0; i<persObjects.length; i++) {
-        persObjects[i].style.display = "block";
-    }
-    // Hide open source project cards:
-    openSrcObjects = document.getElementsByClassName("OpenSource");
-    for (i=0; i<openSrcObjects.length; i++) {
-        openSrcObjects[i].style.display = "none";
-    }
+    for (i=0; i<hideableElements.length; i++) {
+        hideableElements[i].style.display = "none";
+    };
+    // Show selected cards:
+    showableElements = document.getElementsByClassName(showableClassName);
+    for (i=0; i<showableElements.length; i++) {
+        showableElements[i].style.display = "block";
+    };
     // Set correct tab active state:
-    document.getElementById("tab-all-proj").classList.remove("is-active");
-    document.getElementById("tab-pers-proj").classList.add("is-active");
-    document.getElementById("tab-open-source-proj").classList.remove("is-active");
-};
-
-var openSourceProj = document.getElementById("open-source-proj");
-openSourceProj.onclick = function(){
-    console.log("Clicked openSourceProj!");
-    // Hide personal project cards:
-    var i;
-    for (i=0; i<persObjects.length; i++) {
-        persObjects[i].style.display = "none";
-    }
-    // Show open source project cards:
-    openSrcObjects = document.getElementsByClassName("OpenSource");
-    for (i=0; i<openSrcObjects.length; i++) {
-        openSrcObjects[i].style.display = "block";
-    }
-    // Set correct tab active state:
-    document.getElementById("tab-all-proj").classList.remove("is-active");
-    document.getElementById("tab-pers-proj").classList.remove("is-active");
-    document.getElementById("tab-open-source-proj").classList.add("is-active");
+    tabIds = ["tab-all-proj", "tab-pers-proj", "tab-open-source-proj"];
+    for (i=0; i<tabIds.length; i++) {
+        if (tabIds[i] == activeTab) {
+            document.getElementById(tabIds[i]).classList.add("is-active");
+        }
+        else {
+            document.getElementById(tabIds[i]).classList.remove("is-active");
+        };
+    };
 };
